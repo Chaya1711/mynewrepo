4205c7fa9197d56512a41c1d13592f1fa86051b2
Chay1711 <chayajayanu@gmail.com>
2026-02-13 16:19:36 +0530

    first commits for pipeline

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "Example1" {
   ami = "ami-0c1fe732b5494dc14"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    subnet_id = "subnet-01ed0bf481ab409a2"
+    subnet_id = "subnet-01ed0bf481ab409a2"#ami
 }
