2dbff9852c0dfd9307aa690452ce9e5a68e982a1
Chay1711 <chayajayanu@gmail.com>
2026-02-13 16:09:34 +0530

    first commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "Example" {
+resource "aws_instance" "Example1" {
   ami = "ami-0c1fe732b5494dc14"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-01ed0bf481ab409a2"
