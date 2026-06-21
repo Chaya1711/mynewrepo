26a669cd3243697a5fc47f43c593d4c094ecce2e
Chay1711 <chaya.jayanu@gmail.com>
2026-06-21 21:15:56 +0530

    message

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@ provider "aws" {
 resource "aws_instance" "Example1" {
   ami = "ami-0521cb2d60cfbb1a6"  # Specify an appropriate AMI ID
     instance_type = "t3.micro" #type
+    subnet_id = "subnet-0113bac68ba29ff6b"
     }
